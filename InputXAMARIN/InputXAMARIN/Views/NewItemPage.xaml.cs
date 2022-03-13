7af0df703f4afdee5623b1447bdf88c18e483a22
Sultanbek <msultanbek@mail.ru>
2022-03-13 20:09:01 +0400

    First commit

---
+++ InputXAMARIN/InputXAMARIN/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using InputXAMARIN.Models;
+using InputXAMARIN.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace InputXAMARIN.Views
+{
+  public partial class NewItemPage : ContentPage
+  {
+    public Item Item { get; set; }
+
+    public NewItemPage()
+    {
+      InitializeComponent();
+      BindingContext = new NewItemViewModel();
+    }
+  }
+}
