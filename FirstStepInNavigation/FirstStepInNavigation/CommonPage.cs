6f2b7ce4bd67c946b42827c4b24a74f3ae9920fa
Sultanbek <msultanbek@mail.ru>
2022-03-13 20:10:18 +0400

    First commit

---
+++ FirstStepInNavigation/FirstStepInNavigation/CommonPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FirstStepInNavigation
+{
+  class CommonPage : ContentPage
+  {
+    public CommonPage()
+    {
+      Title = "Common Page";
+      Button backButton = new Button
+      {
+        Text = "Назад",
+        HorizontalOptions = LayoutOptions.Center,
+        VerticalOptions = LayoutOptions.Center
+      };
+      backButton.Clicked += BackButton_Click;
+      Content = backButton;
+    }
+    private async void BackButton_Click(object sender, EventArgs e)
+    {
+      await Navigation.PopAsync();
+    }
+
+  }
+}
---
+++ FirstStepInNavigation/FirstStepInNavigation/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirstStepInNavigation
+{
+  public partial class MainPage : ContentPage
+  {
+    public MainPage()
+    {
+      InitializeComponent();
+      Title = "Main Page";
+      Button toCommonPageBtn = new Button
+      {
+        Text = "На обычную страницу",
+        HorizontalOptions = LayoutOptions.Center,
+        VerticalOptions = LayoutOptions.CenterAndExpand
+      };
+      toCommonPageBtn.Clicked += ToCommonPage;
+
+      Button toModalPageBtn = new Button
+      {
+        Text = "На модальную страницу",
+        HorizontalOptions = LayoutOptions.Center,
+        VerticalOptions = LayoutOptions.CenterAndExpand
+      };
+      toModalPageBtn.Clicked += ToModalPage;
+
+      Content = new StackLayout { Children = { toCommonPageBtn, toModalPageBtn } };
+    }
+
+    private async void ToModalPage(object sender, EventArgs e)
+    {
+      await Navigation.PushModalAsync(new ModalPage());
+    }
+    private async void ToCommonPage(object sender, EventArgs e)
+    {
+      await Navigation.PushAsync(new CommonPage());
+    }
+  }
+}
+
